c019b89fbbea00c3f4766d18051975fa2f049bcd
Aashif <asdf7705889@gmail.com>
2025-08-14 21:18:18 +0530

    Git Commited

--- src/com/DSA/program/misc/Pattern.java
+++ src/com/DSA/program/misc/Pattern.java
@@ -22,16 +22,32 @@ public class Pattern {
 			
 			
 			for (int j=1; j<=i; j++) {
-				System.out.println("* ");
+				System.out.print("*");
 				
 			}
+			System.out.print(" ");
+		}
+		for(int i=n-1; i>0; i--) {
+			
+			for (int j=1; j<=i; j++) {
+				System.out.println("*");
+				
+			}
+			
+			System.out.println(" ");
 		}
 		
 		
 		
 	}
-	
-	
+//	
+//	Employee(id, name, email, mobile)
+//	Address (id, employeeId, address1, city, state)
+//	Need a query to get list of unique cities & the number of employees in each city
+////	
+//	
+//	select add.city, count(distinct a.employeeId) AS empCount
+//	from Address add groupby(add.city) order by empCount desc;
 	
 	
 
